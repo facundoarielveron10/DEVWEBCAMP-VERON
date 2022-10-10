f858ffa4f09d8252653295c2df44a7707cdc60fa
Facundo Veron <facu_ve_2016@hotmail.com>
2022-10-10 16:18:07 -0300

    HEADER FINALIZADO

---
+++ views/templates/header.php
@@ -0,0 +1,41 @@
+<!-- Header -->
+<header class="header">
+    <div class="header__contenedor">
+        <!-- Navegacion -->
+        <nav class="header__navegacion">
+            <a href="/registro" class="header__enlace">Registro</a>
+            <a href="/login" class="header__enlace">Iniciar Sesion</a>
+        </nav>
+        <!-- Contenido -->
+        <div class="header__contenido">
+            <!-- Logo -->
+            <a href="/">
+                <h1 class="header__logo">&#60;DevWebCamp /></h1>
+            </a>
+            <!-- Datos -->
+            <p class="header__texto">Febrero 15-16 - 2023</p>
+            <p class="header__texto header__texto--modalidad">En linea - Presencial</p>
+            <!-- Boton Compra -->
+            <a href="/registro" class="header__boton">Comprar Pase</a>
+        </div>
+    </div>
+</header>
+<!-- Barra -->
+<div class="barra">
+    <!-- Contenido -->
+    <div class="barra__contenido">
+        <!-- Logo -->
+        <a href="/">
+            <h2 class="barra__logo">
+                &#60;DevWebCamp />
+            </h2>
+        </a>
+        <!-- Navegacion -->
+        <nav class="navegacion">
+            <a href="/devwebcamp" class="navegacion__enlace">Evento</a>
+            <a href="/paquetes" class="navegacion__enlace">Paquetes</a>
+            <a href="/workshops-conferencias" class="navegacion__enlace">Workshops / Conferencias</a>
+            <a href="/registro" class="navegacion__enlace">Comprar Pase</a>
+        </nav>
+    </div>
+</div>
