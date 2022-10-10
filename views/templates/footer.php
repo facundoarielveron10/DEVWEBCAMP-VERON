1f5c0a167e804a3898d8abd095db27fec0bd252e
Facundo Veron <facu_ve_2016@hotmail.com>
2022-10-10 17:12:10 -0300

    FOOTER FINALIZADO

---
+++ views/templates/footer.php
@@ -0,0 +1,42 @@
+<!-- Footer -->
+<footer class="footer">
+    <!-- Posicion -->
+    <div class="footer__grid">
+        <!-- Contenido -->
+        <div class="footer__contenido">
+            <!-- Logo -->
+            <h3 class="footer__logo">
+                &#60;DevWebCamp />
+            </h3>
+            <!-- Descripcion -->
+            <p class="footer__texto">
+                DevWebCamp es una conferencia para desarroladores de todos los niveles, se lleva a cabo de forma presencial y en linea.
+            </p>
+        </div>
+        <!-- Navegacion -->
+        <nav class="menu-redes"> 
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://twitter.com/Facundo06666081">
+                <span class="menu-redes__ocultar">Twitter</span>
+            </a> 
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://youtube.com/channel/UCe3mUuKfDUj-BSvw5LsCDIA">
+                <span class="menu-redes__ocultar">YouTube</span>
+            </a> 
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://instagram.com/fakundo0o/">
+                <span class="menu-redes__ocultar">Instagram</span>
+            </a> 
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://linkedin.com/in/facundo-veron-380050239/">
+                <span class="menu-redes__ocultar">Linkedin</span>
+            </a> 
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://github.com/facundoarielveron10">
+                <span class="menu-redes__ocultar">GitHub</span>
+            </a>
+        </nav>
+    </div>
+    <!-- Copyright -->
+    <p class="footer__copyright">
+        DevWebCamp
+        <span class="footer__copyright--regular">
+            - Todos los derechos reservados &#169; <?php echo date('Y'); ?>
+        </span>
+    </p>
+</footer>
