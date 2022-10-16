f6196add97062898ed23f81711dcf9eac52e5cd3
Facundo Veron <facu_ve_2016@hotmail.com>
2022-10-15 22:44:39 -0300

    Eventos: Responsive a mobiles

--- views/admin/eventos/index.php
+++ views/admin/eventos/index.php
@@ -8,7 +8,49 @@
     </a>
 </div>
 <!-- Listado de Eventos -->
-<div class="dashboard__contenedor">
+<div class="dashboard__contenedor--mobile">
+    <?php if(!empty($eventos)): ?>
+        <!-- Eventos -->
+        <?php foreach($eventos as $evento): ?>
+            <!-- Nombre del Evento -->
+            <h3 class="dashboard__contenedor--mobile__h3"><?php echo $evento->nombre; ?></h3>
+            <!-- Datos del Evento -->
+            <div class="dashboard__contenedor--mobile__evento">
+                <!-- Tipo -->
+                <label class="dashboard__contenedor--mobile__label">Tipo </label>
+                <p class="dashboard__contenedor--mobile__p"><?php echo $evento->categoria->nombre; ?></p>
+                <!-- Dia y Hora -->
+                <label class="dashboard__contenedor--mobile__label">Dia y Hora</label>
+                <p class="dashboard__contenedor--mobile__p"><?php echo $evento->dia->nombre . ', ' . $evento->hora->hora?></p>
+                <!-- Ponente -->
+                <label class="dashboard__contenedor--mobile__label">Ponente</label>
+                <p class="dashboard__contenedor--mobile__p"><?php echo $evento->ponente->nombre . ' ' . $evento->ponente->apellido ?></p>
+                <!-- Botones Eliminar y Editar -->
+                <div class="dashboard__contenedor--mobile__botones">
+                    <!-- Editar -->
+                    <a class="table__accion table__accion--editar" href="/admin/ponentes/editar?id=<?php echo $evento->id; ?>">
+                        <!-- Icono Editar -->
+                        <i class="fa-solid fa-user-pen"></i>
+                        Editar
+                    </a>
+                    <!-- Eliminar -->
+                    <form class="dashboard__contenedor--mobile__formulario" method="POST" action="/admin/ponentes/eliminar">
+                        <input type="hidden" name="id" value="<?php echo $evento->id ?>">
+                        <button class="table__accion table__accion--eliminar" type="submit">
+                            <!-- Icono Eliminar -->
+                            <i class="fa-solid fa-circle-xmark"></i>
+                                Eliminar
+                        </button>
+                    </form>          
+                </div>
+            </div>
+        <?php endforeach; ?>
+    <?php else: ?>
+        <p class="text-center">No hay Eventos Aún</p>
+    <?php endif; ?>
+</div>
+<!-- Listado de Eventos -->
+<div class="dashboard__contenedor dashboard__contenedor--eventos">
     <?php if(!empty($eventos)): ?>
         <!-- Tabla -->
         <table class="table">
@@ -42,7 +84,7 @@
                         </td>
                         <!-- Ponente -->
                         <td class="table__td">
-                            <?php echo $evento->ponente->nombre . $evento->ponente->apellido ?>
+                            <?php echo $evento->ponente->nombre . ' ' . $evento->ponente->apellido ?>
                         </td>
                         <!-- Boton Editar y Boton Eliminar -->
                         <td class="table__td--acciones">
