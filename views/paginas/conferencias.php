07728c6bfbbe0da0479e86e651d87121e6353a36
Facundo Veron <facu_ve_2016@hotmail.com>
2022-10-17 23:25:26 -0300

    Conferencias Titulos y Descripciones

---
+++ views/paginas/conferencias.php
@@ -0,0 +1,46 @@
+<!-- Agenda -->
+<main class="agenda">
+    <!-- Titulo -->
+    <h2 class="agenda__heading"><?php echo $titulo ?></h2>
+    <!-- Descripcion -->
+    <p class="agenda__descripcion">Talleres y Conferencias dictados por expertos en Desarrollo Web</p>
+
+    <!-- Evento (Conferencias) -->
+    <div class="eventos">
+        <!-- Titulo -->
+        <h3 class="eventos__heading">&lt;Conferencias /></h3>
+        <!-- Fecha -->
+        <p class="evento__fecha">Viernes 5 de Octubre</p>
+        <!-- Listado Conferencias (Viernes) -->
+        <div class="eventos__listado">
+
+        </div>
+
+        <!-- Fecha -->
+        <p class="eventos__fecha">Sabado 6 de Octubre</p>
+        <!-- Listado Conferencias (Sabado) -->
+        <div class="eventos__listado">
+
+        </div>
+    </div>
+
+    <!-- Evento (Workshops) -->
+    <div class="eventos eventos--workshops">
+        <!-- Titulo -->
+        <h3 class="eventos__heading">&lt;Workshops /></h3>
+
+        <!-- Fecha -->
+        <p class="evento__fecha">Viernes 5 de Octubre</p>
+        <!-- Listado Workshops (Viernes) -->
+        <div class="eventos__listado">
+
+        </div>
+
+        <!-- Fecha -->
+        <p class="eventos__fecha">Sabado 6 de Octubre</p>
+        <!-- Listado Workshops (Sabado) -->
+        <div class="eventos__listado">
+
+        </div>
+    </div>
+</main>
